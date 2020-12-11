88a6915111a35bbaa09a97427893d366a07af512
Glen Small <glen.small@microsoft.com>
2020-12-11 16:50:59 +0000

    updating the getPerfmon project

--- C++/grabPerfmonData/grabPerfmonData.cpp
+++ C++/grabPerfmonData/grabPerfmonData.cpp
@@ -1,20 +1,16 @@
-// grabPerfmonData.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// Entrypoint for our application
 
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    /*
+    TODO:  Need to add a class to handle the PDH side if things
+            Need to do something to handle uder input
+            Need to do something to handle reading a config file with counters we want to grab and other stuff
+            Need to handle console output in a formatted way
+            Could add a means of saving the data to mongoDB or another DB store.    
+    */
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
