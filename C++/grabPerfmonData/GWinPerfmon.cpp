d1f71edcd063bab0ba48dc9fe8b83a851e584263
Glen Small <14293295+glensmall@users.noreply.github.com>
2020-12-12 10:51:00 +0000

    adding classes for Perfmon

---
+++ C++/grabPerfmonData/GWinPerfmon.cpp
@@ -0,0 +1,27 @@
+/*
+	Implementation file for the GWinPerfmon Class
+*/
+
+#include "GWinPerfmon.h"
+
+// Default Constructor
+GWinPerfmon::GWinPerfmon()
+{
+	// TODO: Add implementation code here if needed
+}
+
+
+// Default Destructor
+GWinPerfmon::~GWinPerfmon()
+{
+	// TODO: clean up some of the memory thats been allocated
+	
+	
+}
+
+
+// Accessor function to set the machine name
+void GWinPerfmon::setMachineName(char* name)
+{
+	this->machineName = name;
+}
---
+++ C++/grabPerfmonData/GWinPerfmon.h
@@ -0,0 +1,30 @@
+#pragma once
+/*
+	Class that wraps the PDH functions and allows me to handle perfmon data from the specifiec machine
+*/
+
+// includes
+#include <string>
+#include <pdh.h>
+
+
+class GWinPerfmon
+{
+	// Construction
+	GWinPerfmon();
+
+
+	// Destruction
+	~GWinPerfmon();
+
+	// Public Vars
+
+	// Private Vars
+	std::string machineName;
+
+
+	// Public Methods
+	void setMachineName(char* name);
+	
+};
+
