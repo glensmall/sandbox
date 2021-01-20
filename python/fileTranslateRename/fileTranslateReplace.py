17fa34bc88b04786e672baa2a027f145bbdd7db3
Glen Small <14293295+glensmall@users.noreply.github.com>
2021-01-20 22:03:26 +0000

    cleaning up the files

--- python/fileTranslateRename/fileTranslateReplace.py
+++ python/fileTranslateRename/fileTranslateReplace.py
@@ -2,13 +2,8 @@ import os
 from google_trans_new import google_translator 
 
 # root folder from where we will scan
-rootDir = r"C:\Users\glens\Videos\test"
+rootDir = r"C:\Users\glens\Videos\Lucy Cat"
 processedDir = r"C:\Users\glens\Videos\Lucy Cat en"
-
-# specify our source and destination languages.
-# google can do this for us but lets be specific
-srcLang = 'de'
-destLang = 'en'
 
 # create the translator object
 # do this globally to save calling it over and over in the method
@@ -26,11 +21,11 @@ if __name__ == "__main__":
             print(f'Working on [{filename}]\n')
             
             # translate the filename from german to english
-            newFilename = translator.translate(filename, lang_tgt='en')
-                        
+            txt = translator.translate(filename, lang_tgt='en')
+            
+            newFilename = txt.replace('/', '.')
+
             print(f'Translated to [{newFilename}\n')
             
             # now move the file to a new location with the new name
             os.rename(os.path.join(rootDir, filename), os.path.join(processedDir, newFilename))
-            
-            
