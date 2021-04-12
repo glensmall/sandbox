19e7b664c1b1289ae53b20e5f45c50241bc63acb
Glen Small <smallgle@amazon.co.uk>
2021-04-12 20:44:45 +0100

    getting posh-git on mac to work

--- golang/hello-world/helloWorld.go
+++ golang/hello-world/helloWorld.go
@@ -5,5 +5,5 @@ import "fmt"
 func main() {
 	fmt.Println("Hello World - this is golang")
 	fmt.Println("Hello World - this is golang")
-	fmt.Println("Hello from a Macbook !!")
+	fmt.Println("Hello from a Macbook !!!")
 }
