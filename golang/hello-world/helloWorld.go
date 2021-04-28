8112cb65b7c7c8a38b249f2157e9b84b82b23ed3
Glen Small <14293295+glensmall@users.noreply.github.com>
2021-04-28 16:41:38 +0100

    testing my certs on commit

--- golang/hello-world/helloWorld.go
+++ golang/hello-world/helloWorld.go
@@ -6,4 +6,5 @@ func main() {
 	fmt.Println("Hello World - this is golang")
 	fmt.Println("Hello World - this is golang")
 	fmt.Println("Hello from a Macbook !!!")
+	fmt.Println("Hello from my own PC")
 }
